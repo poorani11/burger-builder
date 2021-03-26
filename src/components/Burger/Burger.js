f101959fe61f450de8417c63d5b48f4d8cd6e6fe
poorni1 <poorni@unifytech.com>
2021-03-27 03:25:18 +0530

    Added the basic Burger Builder

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+import classes from './Burger.module.css';
+
+const Burger = (props) => {
+  console.log(props.ingredients);
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
+
+  return (
+    <div className={classes.Burger}>
+      <h1>Burger</h1>
+      <BurgerIngredient type='bread-top' />
+      {transformedIngredients}
+      <BurgerIngredient type='bread-bottom' />
+    </div>
+  );
+};
+
+export default Burger;
